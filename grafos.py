c1ae7516e38681ca32ccd44601a86d42037759b4
DanyHidalgo <danielhl@ufm.edu>
2023-11-09 16:31:16 -0600

    intento de arreglar bug :)

--- grafos.py
+++ grafos.py
@@ -20,6 +20,7 @@ class Grafos:
     def __init__(self):
         self.nodes = []
         self.lista_adyascencia = []
+        self.nodo_a_indice = {}
 
     '''
     def __iter__(self):
@@ -46,27 +47,41 @@ class Grafos:
             self.lista_adyascencia.append(self.nodes[i].arista)
         
 
-    def insert(self, new_node: Node, conection_node: Node):
+    def insert(self, new_node: Node, connection_node: Node):
+        # Verifica si los nodos ya existen en el grafo
+        if new_node not in self.nodes:
+            self.nodes.append(new_node)
+            self.nodo_a_indice[new_node] = len(self.nodes) - 1
+            print(f"Agregado nuevo nodo: {new_node.data} con índice {self.nodo_a_indice[new_node]}")
+        
+        if connection_node not in self.nodes:
+            self.nodes.append(connection_node)
+            self.nodo_a_indice[connection_node] = len(self.nodes) - 1
+            print(f"Agregado nuevo nodo: {connection_node.data} con índice {self.nodo_a_indice[connection_node]}")
 
-        new_node.arista.append(conection_node.data)
-        conection_node.arista.append(new_node.data)
-        self.nodes.append(new_node)
-        self.nodes.append(conection_node)
+        new_node.arista.append(connection_node.data)
+        connection_node.arista.append(new_node.data)
 
 
 
     def lista_adyacencia_a_matriz(self, lista_adyacencia):
-        num_nodos = len(lista_adyacencia)
+        num_nodos = len(self.nodes)
 
         # Crea una matriz de adyacencia llena de ceros
         matriz_adyacencia = np.zeros((num_nodos, num_nodos))
 
         # Rellena la matriz de adyacencia con 1s donde hay conexiones en la lista de adyacencia
         for nodo, vecinos in enumerate(lista_adyacencia):
-            for vecino in vecinos:
-                matriz_adyacencia[nodo][vecino] = 1
+            if nodo in self.nodo_a_indice:
+                for vecino in vecinos:
+                    if vecino in self.nodo_a_indice:
+                        matriz_adyacencia[self.nodo_a_indice[nodo]][self.nodo_a_indice[vecino]] = 1
+            else:
+                # Aquí puedes manejar el caso donde un nodo no está en el diccionario (opcional)
+                print(f"El nodo {nodo} no está en el diccionario")
 
         # Crea un DataFrame de Pandas a partir de la matriz de adyacencia
         df = pd.DataFrame(matriz_adyacencia, index=range(num_nodos), columns=range(num_nodos))
 
         return df
+
--- main.py
+++ main.py
@@ -17,12 +17,20 @@ print(node_a)
 ll = Grafos()
 
 # Insert at beginning
-ll.insert(node_a,node_b)
+ll.insert(node_a, node_b)
+ll.insert(node_b, node_c)
+ll.insert(node_c, node_d)
+ll.insert(node_d, node_a)
+ll.insert(node_a, node_e)
+ll.insert(node_b, node_f)
 
 print(node_a)
 print(node_b)
 print(ll.nodes)
 ll.lista_adyasecente()
-n = ll.lista_adyascencia
-print(n)
-ll.lista_adyacencia_a_matriz(n)
+lista_adyacencia = ll.lista_adyascencia
+print(lista_adyacencia)
+matriz_adyacencia = ll.lista_adyacencia_a_matriz(lista_adyacencia)
+
+print(matriz_adyacencia)
+
